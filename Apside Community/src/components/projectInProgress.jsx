84974801db2e874acc9989a016797f2f9adbf94c
francois_pobelle <francoispobelle@hotmail.fr>
2022-06-30 00:21:21 +0200

    table projects en dur

--- Apside Community/src/components/projectInProgress.jsx
+++ Apside Community/src/components/projectInProgress.jsx
@@ -13,20 +13,21 @@ function createData(name, customer, project_manager, start_date, estimed_end, te
 
 const rows = [
   createData("Bamity","Twinder","Hinze Guyonnet","19/07/2021","25/06/2021","php"),
+  createData("Heliport view","Eurocopter","Renata Fishpond","19/07/2020","25/06/2020","c++"),
   createData("Airbus A380","Airbus","Almeria Windley","19/07/2020","25/06/2020","java"),
-  createData("Aerospace Bird","","El Ouldcott","19/02/2022","01/04/2022","php"),
+  createData("Aerospace Bird","Renault Future","El Ouldcott","19/02/2022","01/04/2022","php"),
   createData("Thales Satelis","Thales","Gery Dun","02/07/2019","12/012/2020","html"),
   createData("Heliport view","Eurocopter","Renata Fishpond","19/07/2020","25/06/2020","c++"),
 ];
 
-function projectInProgress() {
+function ProjectInProgress() {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
             <TableCell>PROJECT IN PROGRESS</TableCell>
-            <TableCell align="right">Name Project</TableCell>
+           
             <TableCell align="right">Customer</TableCell>
             <TableCell align="right">Project Manager</TableCell>
             <TableCell align="right">Start date</TableCell>
@@ -43,10 +44,11 @@ function projectInProgress() {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.customer}</TableCell>
+              <TableCell align="right">{row.project_manager}</TableCell>
+              <TableCell align="right">{row.start_date}</TableCell>
+              <TableCell align="right">{row.estimed_end}</TableCell>
+              <TableCell align="right">{row.techno}</TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -55,4 +57,4 @@ function projectInProgress() {
   )
 }
 
-export default projectInProgress
+export default ProjectInProgress
